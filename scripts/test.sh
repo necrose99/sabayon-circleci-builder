25ccc18e7f216b7bd40b83d0b9d284dd618846e2
mudler <mudler@dark-lab.net>
2015-11-14 19:04:36 +0100

    try to remount part of directories

--- scripts/test.sh
+++ scripts/test.sh
@@ -3,26 +3,11 @@
 set -o nounset
 set -o errexit
 
-
-git clone git://github.com/Sabayon/build.git ~/sabayon-build
-pushd ~/sabayon-build/conf/intel/portage
-mv make.conf.amd64 make.conf
-mv package.env.amd64 package.env
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.keywords ./package.keywords/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.unmask ./package.unmask/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.env ./package.env/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.mask ./package.mask/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.use ./package.use/
-cat "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.make >> ./make.conf
-popd
-
-ls -liah "$HOME/$CIRCLE_PROJECT_REPONAME"
-ls -liah ~/sabayon-build
 docker run -e BUILDER_PROFILE -e BUILDER_JOBS -e PRESERVED_REBUILD -e EMERGE_DEFAULTS_ARGS \
--v $CIRCLE_ARTIFACTS/:/usr/portage/packages \
--v $HOME/sabayon-build:/opt/sabayon-build \
--t sabayon/builder-amd64 $BUILD_ARGS
-
-#-v $HOME/portage:/opt/sabayon-build/conf/intel/portage \
-
-#
+		-v $CIRCLE_ARTIFACTS/:/usr/portage/packages \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.keywords:/opt/sabayon-build/conf/intel/portage/package.keywords/custom.keywords \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.unmask:/opt/sabayon-build/conf/intel/portage/package.unmask/custom.unmask \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.env:/opt/sabayon-build/conf/intel/portage/package.env/custom.env \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.mask:/opt/sabayon-build/conf/intel/portage/package.mask/custom.mask \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.use:/opt/sabayon-build/conf/intel/portage/package.use/custom.use \
+		-t sabayon/builder-amd64 $BUILD_ARGS
