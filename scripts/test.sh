7fd7ca2db0542aec2d1c0ab0f6170d4369797170
mudler <mudler@dark-lab.net>
2015-11-14 16:00:32 +0100

    [scripts/test] docker wants abs path

--- scripts/test.sh
+++ scripts/test.sh
@@ -4,14 +4,26 @@ set -o nounset
 set -o errexit
 
 if [ -n "$BUILD_ARGS" ]; then
-docker run \
-	-e BUILDER_PROFILE -e BUILDER_JOBS -e PRESERVED_REBUILD -e EMERGE_DEFAULTS_ARGS \
+
+	git clone git://github.com/Sabayon/build.git ~/sabayon-build
+	mv ~/sabayon-build/conf/intel/portage ~/portage
+	pushd ~/portage
+	mv make.conf.amd64 make.conf
+	mv package.env.amd64 package.env
+	popd
+	cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.keywords ~/portage/package.keywords/
+	cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.unmask ~/portage/package.unmask/
+	cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.env ~/portage/package.env/
+	cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.mask ~/portage/package.mask/
+	cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.use ~/portage/package.use/
+	cat "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.make >> ~/portage/make.conf
+	pwd
+	ls -liah "$HOME/$CIRCLE_PROJECT_REPONAME"
+	echo "$HOME/$CIRCLE_PROJECT_REPONAME"
+	docker run -e BUILDER_PROFILE -e BUILDER_JOBS -e PRESERVED_REBUILD -e EMERGE_DEFAULTS_ARGS \
 	-v $CIRCLE_ARTIFACTS/:/usr/portage/packages \
-	-v custom.mask:/etc/portage/package.mask/custom.mask \
-        -v custom.use:/etc/portage/package.use/custom.use \
-        -v custom.keywords:/etc/portage/package.keywords/custom.keywords \
-        -v custom.unmask:/etc/portage/package.unmask/custom.unmask \
-        -v custom.env:/etc/portage/package.env/custom.env \
+	-v $HOME/portage:/opt/sabayon-build/conf/intel/portage \
 	-t sabayon/builder-amd64 $BUILD_ARGS
 fi
 
+#	-v $HOME/portage:/etc/portage
