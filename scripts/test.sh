bec89c65c1c0f53d52e2a204b131c3e0c23a5d91
mudler <mudler@dark-lab.net>
2015-11-14 19:20:27 +0100

    moving custom files inside conf/ directory

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,9 +5,9 @@ set -o errexit
 
 docker run -e BUILDER_PROFILE -e BUILDER_JOBS -e PRESERVED_REBUILD -e EMERGE_DEFAULTS_ARGS \
 		-v $CIRCLE_ARTIFACTS/:/usr/portage/packages \
-		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.keywords:/opt/sabayon-build/conf/intel/portage/package.keywords/custom.keywords \
-		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.unmask:/opt/sabayon-build/conf/intel/portage/package.unmask/custom.unmask \
-		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.env:/opt/sabayon-build/conf/intel/portage/package.env/custom.env \
-		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.mask:/opt/sabayon-build/conf/intel/portage/package.mask/custom.mask \
-		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.use:/opt/sabayon-build/conf/intel/portage/package.use/custom.use \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/conf/custom.keywords:/opt/sabayon-build/conf/intel/portage/package.keywords/custom.keywords \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/conf/custom.unmask:/opt/sabayon-build/conf/intel/portage/package.unmask/custom.unmask \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/conf/custom.env:/opt/sabayon-build/conf/intel/portage/package.env/custom.env \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/conf/custom.mask:/opt/sabayon-build/conf/intel/portage/package.mask/custom.mask \
+		-v "$HOME/$CIRCLE_PROJECT_REPONAME"/conf/custom.use:/opt/sabayon-build/conf/intel/portage/package.use/custom.use \
 		-t sabayon/builder-amd64 $BUILD_ARGS
