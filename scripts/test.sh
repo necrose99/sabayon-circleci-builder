2a82d493eec2e03fdcbee6e230b4bf5c6a318f7f
mudler <mudler@dark-lab.net>
2015-11-14 18:20:48 +0100

    [script] replacing symlink present in container. workaround

--- scripts/test.sh
+++ scripts/test.sh
@@ -5,25 +5,22 @@ set -o errexit
 
 
 git clone git://github.com/Sabayon/build.git ~/sabayon-build
-mv ~/sabayon-build/conf/intel/portage ~/portage
-pushd ~/portage
+pushd ~/sabayon-build/conf/intel/portage
 mv make.conf.amd64 make.conf
 mv package.env.amd64 package.env
+cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.keywords ./package.keywords/
+cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.unmask ./package.unmask/
+cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.env ./package.env/
+cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.mask ./package.mask/
+cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.use ./package.use/
+cat "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.make >> ./make.conf
 popd
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.keywords ~/portage/package.keywords/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.unmask ~/portage/package.unmask/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.env ~/portage/package.env/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.mask ~/portage/package.mask/
-cp -rfv "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.use ~/portage/package.use/
-cat "$HOME/$CIRCLE_PROJECT_REPONAME"/custom.make >> ~/portage/make.conf
-
-pwd
 
 ls -liah "$HOME/$CIRCLE_PROJECT_REPONAME"
-echo "$HOME/$CIRCLE_PROJECT_REPONAME"
+ls -liah ~/sabayon-build
 docker run -e BUILDER_PROFILE -e BUILDER_JOBS -e PRESERVED_REBUILD -e EMERGE_DEFAULTS_ARGS \
 -v $CIRCLE_ARTIFACTS/:/usr/portage/packages \
--v $HOME/portage:/etc/portage \
+-v $HOME/sabayon-build:/opt/sabayon-build \
 -t sabayon/builder-amd64 $BUILD_ARGS
 
 #-v $HOME/portage:/opt/sabayon-build/conf/intel/portage \
